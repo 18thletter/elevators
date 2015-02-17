0ba3b3c6655648c09b1fbe1af59ec4c55e4cb91d
Ray Lew <ray@icracked.com>
2015-02-17 09:43:10 -0800

    Made some changes, but may have broken it

--- elevator.js
+++ elevator.js
@@ -1,134 +1,151 @@
 {
-  elevatorIsFull: function(elevator) {
-    return elevator.loadFactor() > 0.9;
-  },
-  elevatorIsIdle: function(elevator) {
-    return elevator.destinationQueue === 0;
-  },
-  floorsNeedingPickup: [],
-  floorsNeedingPickupGoingUp: [],
-  floorsNeedingPickupGoingDown: [],
   init: function(elevators, floors) {
-    var bottomFloor = 0;
-    var topFloor = floors.length - 1;
+    var bottomFloor = 0,
+    topFloor = floors.length - 1,
+    elevatorIsFull = function(elevator) {
+      return elevator.loadFactor() > 0.9;
+    },
+    elevatorIsIdle = function(elevator) {
+      return elevator.destinationQueue === [NaN];
+    },
+    floorHasAnElevatorGoingToIt = function(floorNum) {
+      return _.some(elevators, function(elevator) {
+        return _.contains(elevator.destinationQueue, floorNum);
+      });
+    },
+    goToFloor = function(elevator, floorNum) {
+      elevator.goToFloor(floorNum);
+      var firstQueuedFloor = elevator.destinationQueue[0];
+      var currentFloor = elevator.currentFloor();
+      if (currentFloor > firstQueuedFloor) {
+        elevator.goingDownIndicator(true);
+        elevator.goingUpIndicator(false);
+      } else if (currentFloor < firstQueuedFloor) {
+        elevator.goingDownIndicator(false);
+        elevator.goingUpIndicator(true);
+      }
+      console.log('Elevator ' + elevator.num + ' sent to floor ' + floorNum);
+    },
+    floorsNeedingPickup = [],
+    floorsNeedingPickupGoingUp = [],
+    floorsNeedingPickupGoingDown = [],
+    addToSet = function(set, val) {
+      set.push(val);
+      set.sort();
+      set = _.uniq(set, true);
+    },
+    findClosestFloor = function(elevator) {
+      var currentFloor = elevator.currentFloor();
+      var differences = [];
+      floorsNeedingPickup.forEach(function(floor) {
+        differences.push({
+          floor: floor,
+          abs: Math.abs(currentFloor - floor)
+        });
+      });
+      differences.sort(function(diff1, diff2) {
+        return diff1.abs < diff2.abs;
+      });
+      if (differences.length === 0) {
+        return currentFloor;
+      } else {
+        return differences[0].floor;
+      }
+    };
 
     // set the elevator events
-    for (var i = 0; i < elevators.length; i++) {
+    var elevatorNumber = 0;
+    elevators.forEach(function(elevator) {
+      // put a number on the elevator
+      elevator.num = elevatorNumber++;
+
       // when an elevator is idle, stop it
-      elevators[i].on('idle', function() {
-        this.stop();
+      elevator.on('idle', function() {
+        console.log('Elevator ' + elevator.num + ' is idle');
         this.goingUpIndicator(false);
         this.goingDownIndicator(false);
 
-        // check the floors needing pick up
-        var nextFloor = floorsNeedingPickup.shift();
-        if (nextFloor === undefined) {
-          return;
-        }
-
-        if
+        goToFloor(elevator, findClosestFloor(elevator));
       });
-      elevators[i].on('floor_button_pressed', function(floorNum) {
+
+      elevator.on('floor_button_pressed', function(floorNum) {
         // is the floor pressed above or below the current floor?
-        var requestedFloorIsAbove = floorNum > this.currentFloor();
-        // if the elevator is idle, just go to that floor
-        if (elevatorIsIdle(this)) {
-          this.destinationQueue = [floorNum];
-          if (requestedFloorIsAbove) {
-            this.goingUpIndicator(true);
-            this.goingDownIndicator(false);
-          }
-          this.checkDestinationQueue();
-        } else if (this.goingUpIndicator() === true && )
-        if(!_.contains(this.destinationQueue, floorNum) && ) {
+        //var requestedFloorIsAbove = floorNum > this.currentFloor();
+        //// if the elevator is idle, just go to that floor
+        //if (elevatorIsIdle(this)) {
+          //this.destinationQueue = [floorNum];
+          //if (requestedFloorIsAbove) {
+            //this.goingUpIndicator(true);
+            //this.goingDownIndicator(false);
+          //}
+          //this.checkDestinationQueue();
+        //} else if (this.goingUpIndicator() === true && )
+        //if(!_.contains(this.destinationQueue, floorNum) && ) {
 
-        }
+        //}
       });
-      elevators[i].on('stopped_at_floor', function(floorNum) {
-        console.log('Elevator ' + i + ' stopped at floor ' + floorNum);
-        // clear the pickup queues
-        floorsNeedingPickup = _.without(floorsNeedingPickup, floorNum);
-        floorsNeedingPickupGoingUp = _.without(floorsNeedingPickupGoingUp, floorNum);
-        floorsNeedingPickupGoingDown = _.without(floorsNeedingPickupGoingDown, floorNum);
+
+      elevator.on('stopped_at_floor', function(floorNum) {
+        console.log('Elevator ' + elevator.num + ' stopped at floor ' + floorNum);
+        // clear the sets
       });
-    }
+    });
 
     // set the floor events
-    for (var floorNum = 0; floorNum < floors.length; floorNum++) {
-      floors[floorNum].on('up_button_pressed', function() {
+    floors.forEach(function(floor) {
+      var floorNum = floor.floorNum();
+      floor.on('up_button_pressed', function() {
         console.log('Up button pressed on floor ' + floorNum);
-        floorsNeedingPickup.push(floorNum);
-        floorsNeedingPickup.sort();
-        floorsNeedingPickup = _.uniq(floorsNeedingPickup, true);
-        floorsNeedingPickupGoingUp.push(floorNum);
-        floorsNeedingPickupGoingUp.sort();
-        floorsNeedingPickupGoingUp = _.uniq(floorsNeedingPickupGoingUp, true);
+        addToSet(floorsNeedingPickup, floorNum);
+        addToSet(floorsNeedingPickupGoingUp, floorNum);
 
         // do we even need to send an elevator?
         // if an elevator is already heading there, do nothing
-        if (_.some(elevators, function(elevator) {
-          return _.contains(elevator.destinationQueue, floorNum);
-        })) {
+        if (floorHasAnElevatorGoingToIt(elevators, floorNum)) {
           return;
         }
 
         // are any elevators idle?
-        // tell the first non-idle elevator to go to that floor
+        // tell the first idle elevator to go to that floor
         var firstIdleElevator = _.find(elevators, function(elevator) {
           return elevatorIsIdle(elevator);
         });
-        if (firstIdleElevator !== undefined) {
-          firstIdleElevator.goToFloor(floorNum);
-          if (floorNum < firstIdleElevator.currentFloor()) {
-            firstIdleElevator.goingDownIndicator(true);
-          } else {
-            firstIdleElevator.goingUpIndicator(true);
-          }
-          console.log('Idle elevator sent to floor ' + floorNum);
+        if (firstIdleElevator === undefined) {
           return;
         }
+        goToFloor(firstIdleElevator, floorNum);
 
         // there are no idle elevators at this point
         // the idle or passing_floor events will handle it
       });
-      floors[floorNum].on('down_button_pressed', function() {
+
+      floor.on('down_button_pressed', function() {
         console.log('Down button pressed on floor ' + floorNum);
-        floorsNeedingPickup.push(floorNum);
-        floorsNeedingPickup.sort();
-        floorsNeedingPickup = _.uniq(floorsNeedingPickup, true);
-        floorsNeedingPickupGoingDown.push(floorNum);
-        floorsNeedingPickupGoingDown.sort();
-        floorsNeedingPickupGoingDown = _.uniq(floorsNeedingPickupGoingDown, true);
+        addToSet(floorsNeedingPickup, floorNum);
+        addToSet(floorsNeedingPickupGoingDown, floorNum);
 
         // do we even need to send an elevator?
         // if an elevator is already heading there, do nothing
-        if (_.some(elevators, function(elevator) {
-          return _.contains(elevator.destinationQueue, floorNum);
-        })) {
+        if (floorHasAnElevatorGoingToIt(elevators, floorNum)) {
           return;
         }
 
         // are any elevators idle?
-        // tell the first non-idle elevator to go to that floor
+        // tell the first idle elevator to go to that floor
         var firstIdleElevator = _.find(elevators, function(elevator) {
           return elevatorIsIdle(elevator);
         });
-        if (firstIdleElevator !== undefined) {
-          firstIdleElevator.goToFloor(floorNum);
-          if (floorNum > firstIdleElevator.currentFloor()) {
-            firstIdleElevator.goingUpIndicator(true);
-          } else {
-            firstIdleElevator.goingDownIndicator(true);
-          }
-          console.log('Idle elevator sent to floor ' + floorNum);
+        if (firstIdleElevator === undefined) {
           return;
         }
+        goToFloor(firstIdleElevator, floorNum);
 
         // there are no idle elevators at this point
         // the idle or passing_floor events will handle it
       });
-    }
+    });
   },
+
   update: function(dt, elevators, floors) {
     // We normally don't need to do anything here
   }
